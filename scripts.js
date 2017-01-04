acf2079b8640776566db7f0929dda2ff6a7bb0c1
noahthecodeninja <noahcapucillishata@uagateway.org>
2017-01-04 20:35:42 +0000

    make Robbie sad

--- scripts.js
+++ scripts.js
@@ -1,7 +1,30 @@
-$(document).ready({
+$(document).ready(function(){
 	// Varialbes Used for Logic Go Below:
-
+    var list =[];
 	// Your Code Goes Here!
-
-
+    $("#add").click(function(){
+            if($("#input").val() != ""){
+            list.push($("#input").val());
+            $("#input").val("");
+            updateList();
+        }
+    });
+    function updateList(){
+        $("ol").html("");
+        var count = 0;
+        list.forEach(function(d,i){ 
+           $("ol").append("<li data-index = '" + i + "'>" + d + '<button class = "delete" data-index = "' + i + '">-</button></li>' );
+        });
+    $(".delete").click(function(){
+        list.splice(parseInt($(this).data("index")), 1);
+        updateList();
+        });
+    $("li").click(function(){
+        if($("#input").val() != ""){
+            list[$(this).data("index")] = $("input").val();
+            updateList();
+            $("#input").val("");
+        }
+    });    
+    }
 });
